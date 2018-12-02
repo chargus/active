9e21da96f341ba8b3fb61d70627fb2c5bc097a47
Cory Hargus <cory.hargus@gmail.com>
2018-12-02 00:19:40 -0800

    Experiment with submission options

--- submit.py
+++ submit.py
@@ -7,15 +7,18 @@ import subprocess
 import shlex
 import numpy as np
 
+cmd = ('qsub -q all.q -pe orte 1 -S /bin/bash -N gp_mass_umb -pe mpi 1 -cwd '
+       '-e log -o log -m be -M hargus@berkeley.edu '
+       'run.py  {} -n {} --nframes {} --eta {}')
+cmd = 'qsub -q all.q -cwd -V run.py {} -n {} --nframes {} --eta {}'
 
 etas = np.linspace(0, 5, 21)
 ns = [40, 100, 400, 1000]
 nframes = [2000, 500, 300, 300]
-for n in ns:
+for n, nframes in zip(ns, nframes):
     for eta in etas:
-        path = './simdir/' + 'eta_{}n_{}'.format(eta, n)
-        cmd = 'qsub -q all.q run.py {} -n {} --nframes {} --eta {}'
-        cmd = cmd.format(path, n, nframes, eta)
-        # out = subprocess.check_output(shlex.split(cmd))
-        print cmd
+        path = 'sim/eta_{}n_{}'.format(eta, n)
+        fcmd = cmd.format(path, n, nframes, eta)
+        # out = subprocess.check_output(shlex.split(fcmd))
+        print fcmd
         # out
