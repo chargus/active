9e9499544f34ad5dc49965dfe6450f0e317fdc36
Cory Hargus <cory.hargus@gmail.com>
2018-12-02 10:56:58 -0800

    Add run.job for GSD to interpret in job submission

--- submit.py
+++ submit.py
@@ -7,10 +7,10 @@ import subprocess
 import shlex
 import numpy as np
 
-cmd = ('qsub -q all.q -pe orte 1 -S /bin/bash -N gp_mass_umb -pe mpi 1 -cwd '
-       '-e log -o log -m be -M hargus@berkeley.edu '
-       'run.py  {} -n {} --nframes {} --eta {}')
-cmd = 'qsub -q all.q -cwd -V run.py {} -n {} --nframes {} --eta {}'
+# cmd = ('qsub -q all.q -pe orte 1 -S /bin/bash -N gp_mass_umb -pe mpi 1 -cwd '
+#        '-e log -o log -m be -M hargus@berkeley.edu '
+#        'run.py  {} -n {} --nframes {} --eta {}')
+cmd = 'qsub -q all.q -cwd -V run.job {} -n {} --nframes {} --eta {}'
 
 etas = np.linspace(0, 5, 21)
 ns = [40, 100, 400, 1000]
@@ -19,6 +19,5 @@ for n, nframes in zip(ns, nframes):
     for eta in etas:
         path = 'sim/eta_{}n_{}'.format(eta, n)
         fcmd = cmd.format(path, n, nframes, eta)
-        # out = subprocess.check_output(shlex.split(fcmd))
+        out = subprocess.check_output(shlex.split(fcmd))
         print fcmd
-        # out
